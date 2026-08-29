9a5373d46815681eb094b12f5556eb7cf4fb08de
Thakurakhandrajput <157279374+Thakurakhandrajput@users.noreply.github.com>
2026-08-29 09:11:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.set("view", path.join(__dirname, "view"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 const CATEGORIES = [
